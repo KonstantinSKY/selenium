46b4fe99a6ea2c272b6c5c513edb7a667bac0bed
SKY <sky012877@gmail.com>
2023-02-09 22:17:14 -0800

    page.py - Modified :

--- page.py
+++ page.py
@@ -43,88 +43,31 @@ class Page(Selen):
         print("ELEMENTS", self.ELEMENTS)
 
     def find(self):
-        self.print_arg()
         self.element = self.drv
-        for cmd in self.object:
-            print(cmd)
-            if cmd[2]:
-                self.find_elems(cmd[0], cmd[1])
+        for obj in self.object:
+            print(obj)
+            if 'many' in obj and obj['many']:
+                self.find_elems(obj['by'], obj['attr'])
             else:
-                self.find_elem(cmd[0], cmd[1])
-
-    def click_chain(self):
-        actions = ActionChains(self.drv)
-        self.act_chain.move_to_element(self.elem)
-        self.act_chain.pause(0.5)  # TODO Random
-        eval("self.act_chain.click(self.elem)")
-        self.act_chain.perform()
-        sleep(2)
-        print("Clicked")
-
-
-    def send_key_chain(self):
-        #actions = ActionChains(self.drv)
-        self.act_chain.move_to_element(self.elem)
-        self.act_chain.pause(0.5)  # TODO Random
-        self.act_chain.click(self.elem)
-        print("self.acts", self.acts)
-        self.act_chain.send_keys(self.acts["data"])
-        self.act_chain.perform()
-        sleep(2)
-        print("Sent key")
-
-
-
-    def click(self):
-        self.find()
-        self.click_chain()
-        #TODO Reload if sign_in
-    def send_key(self, obj):
-        print(obj)
-        self.obj = obj if obj is not None else self.obj
-        print(obj)
-        self.find(obj)
-        self.send_key_chain()
+                self.find_elem(obj['by'], obj['attr'])
 
     def start(self, _action):
-        actions = self.actions[_action]
+        actions = self.ACTIONS[_action]
         for action in actions:
-            self.actions = action
-            self.object = action["obj"]
+            self.action = action
+            self.object = self.OBJECTS[action["obj"]]
+            self.element = self.ELEMENTS[action['obj']]
+            self.data = action["data"] if "data" in action else ""
             command = action['cmd']
             print("CMD", command)
-            eval("self." + command)
-
-
-            print("ACTION", action)
-            if action["cmd"] == "click":
-                self.object = action["obj"]
-                self.click()
-
-            if action["cmd"] == "send_keys":
-                print("action", action)
-                self.acts = action
-                self.send_key(action["obj"])
-            sleep(5)
+            eval("self." + command + "()")
+            self.get_all_elements()
 
 
 
 if __name__ == '__main__':
     page = Page(main)
-
-
-    # "opts = Options()
-    # "opts.add_argument('--disable-blink-features=AutomationControlled')
-    # "driver = webdriver.Chrome(options=opts)  # Optional argument, if not specified will search path.
-    # "driver.get('https://www.google.com/')
-    #driver.get('https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html')
-    #sleep(100)
-    # driver.find_element()
-    # page.start("click_sign_in")
-    # page.start("insert_email_login")
-    # page.drv.find_element("tag name", "a")
-    #page.sign_button = [["xpath", '//*[@id="gb"]/div/div[2]/a', False], ]
-    #page.click(page.sign_button)
-    #print(page.drv.current_url)
+    page.start("input_search_data")
+    #page.start("click_sign_in")
     while True:
         pass
